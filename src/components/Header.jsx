21277e42c3d8e58d31b8eec96714377e68d14d7d
Kennedy <kenrichshiku@gmail.com>
2024-02-21 22:14:12 +0300

    Added nav items and icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import logo from './../assets/Images/logo.png'
-
+import { HiHome, HiMagnifyingGlass, HiStar, HiPlayCircle, HiTv} from "react-icons/hi2"
+import { HiPlus, HiDotsVertical } from 'react-icons/hi' 
+import HeaderItem from './HeaderItem'
 function Header() {
+  const HOME = 'Home';
+  const SEARCH = 'Search';
+  const WATCHLIST = 'Watchlist';
+  const ORIGINALS = 'Originals';
+  const MOVIES = 'Movies';
+  const SERIES = 'Series';
+
+  const menu = [
+    {
+      name: HOME,
+      icon: HiHome
+    },
+    {
+      name: SEARCH,
+      icon: HiMagnifyingGlass
+    },
+    {
+      name: WATCHLIST,
+      icon: HiPlus
+    },
+    {
+      name: ORIGINALS,
+      icon: HiStar
+    },
+    {
+      name: MOVIES,
+      icon: HiPlayCircle
+    },
+    {
+      name: SERIES,
+      icon: HiTv
+    },
+  ]
+
   return (
     <div>
       <img src={logo} alt='logo' className='w-[80px] md:w-[115px] object-cover'/>
+      {menu.map((item)=>(
+        <HeaderItem name={item.name} Icon={item.icon}/>
+      ))}
     </div>
   )
 }
